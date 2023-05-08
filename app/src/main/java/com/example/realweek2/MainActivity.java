02306370978daf678a4094f22f61811fcc12d4cc
Thu Ta <s10242515@connect.np.edu..sg>
2023-05-08 11:42:28 +0800

    k

--- app/src/main/java/com/example/realweek2/MainActivity.java
+++ app/src/main/java/com/example/realweek2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +22,13 @@ public class MainActivity extends AppCompatActivity {
     boolean followed = false;
     public void onFollowClick (View v){
         if (followed) {
-
+            Toast.makeText(getBaseContext(),"Unfollowed", Toast.LENGTH_SHORT).show();
             Button button = (Button) findViewById(R.id.btnFollow);
             button.setText("Follow");
             followed = false;
 
         } else {
-
+            Toast.makeText(getBaseContext(),"Followed", Toast.LENGTH_SHORT).show();
             Button button = (Button) findViewById(R.id.btnFollow);
             button.setText("Unfollow");
             followed = true;
